c2640b8e19facfe5d048e3b35f804b55ceaa26a6
Rohan Hossain <asrohan94@gmail.com>
2026-03-27 19:23:22 +0600

    finish

--- my-project/src/pages/Home.jsx
+++ my-project/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import OurPolicy from '../components/OurPolicy'
-import BestSeller from '../components/Bestseller'
+import BestSeller from '../components/Homepage'
 import LatestCollection from '../components/LatestCollectons'
 
 
