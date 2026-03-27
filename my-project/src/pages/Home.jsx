16d52683c9bdafa970c2b9253cc9b8b78532ec60
Rohan Hossain <asrohan94@gmail.com>
2026-03-27 19:26:24 +0600

    finish

--- my-project/src/pages/Home.jsx
+++ my-project/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import OurPolicy from '../components/OurPolicy'
-import BestSeller from '../components/Homepage'
+import HomePage from '../components/Homepage'
 import LatestCollection from '../components/LatestCollectons'
 
 
@@ -13,9 +13,8 @@ const Home = () => {
       
       <Hero/>
       <LatestCollection/>
-      <BestSeller/>
       <OurPolicy/>
-      
+      <HomePage/>
     </div>
   )
 }
